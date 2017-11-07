daff3bdc198a4eb5bb7eda5aceba83a44ea1d5a1
Justin Doty <justindoty12@gmail.com>
2017-11-06 20:32:26 -0600

    initial commit, UI-view, Map-setup

---
+++ pixel-city/Controllers/MapVC.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Justin  Doty on 11/5/17.
+//  Copyright © 2017 Justin  Doty. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+import MapKit
+import CoreLocation
+
+@IBDesignable
+class MapVC: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+    }
+
+   
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+    
+
+}
+
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
